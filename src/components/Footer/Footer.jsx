7cfbcf5c3ed360e8944cfecbc2cb62a02b49bcfd
GAURAV SINGH <gs1663221@gmail.com>
2024-08-28 01:37:53 +0530

    Add Files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer className="bg-body-tertiary text-center text-lg-start">
+  <div className="text-center p-3" >
+    © 2024 Copyright:
+    <p>Gaurav Singh</p>
+  </div>
+</footer>
+    
+    </div>
+  )
+}
+
+export default Footer
